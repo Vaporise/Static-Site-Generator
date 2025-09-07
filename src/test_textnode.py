7536cc4338bb69ac2e7dbf1f2997e1d8fc6101cf
Vaporise <draxdragonfire@googlemail.com>
2025-09-08 00:14:26 +0900

    working on unit test

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -3,11 +3,19 @@ import unittest
 from textnode import TextNode, TextType
 
 
-class TestTextNode(unittest.TestCase):
+class TestTextNode(unittest.TestCase): # Unit test to check if 2 nodes that are equal match
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
+
+    def test_noteq(self): #Unit test to check if not equal specifically text type
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.CODE)
+        self.assertNotEqual(node, node2)
+
+    def test_codeeq(self):
+        node = 
 
 
 if __name__ == "__main__":
