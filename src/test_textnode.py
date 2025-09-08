1a1d32d60c754b80d799676ebd886a1c2ce81e31
Vaporise <draxdragonfire@googlemail.com>
2025-09-08 13:57:28 +0900

    Added some more unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -14,9 +14,19 @@ class TestTextNode(unittest.TestCase): # Unit test to check if 2 nodes that are 
         node2 = TextNode("This is a text node", TextType.CODE)
         self.assertNotEqual(node, node2)
 
-    def test_codeeq(self):
-        node = 
+    def test_codeeq(self): # Test for TextType Code being equal
+        node = TextNode("This is a text node", TextType.CODE)
+        node2 = TextNode("This is a text node", TextType.CODE)
+        self.assertEqual(node, node2)
 
+    def test_unequaltext(self): # Test for text being different
+        node = TextNode("This is a dog node", TextType.TEXT)
+        node2 = TextNode("This is a nose node", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_emptyurl(self): # Test if the url is empty that none is produced
+        node = TextNode("This is a node", TextType.LINK, None)
+        self.assertIsNone(node.url)
 
 if __name__ == "__main__":
     unittest.main()
