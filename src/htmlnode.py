c35e631cd577a7748ac8241925d00cfa1f945d4d
Vaporise <draxdragonfire@googlemail.com>
2025-09-09 11:07:33 +0900

    Added parent node class with some tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,7 +33,25 @@ class LeafNode(HTMLNode):
             raise ValueError("Invalid HTML: no value")
         if self.tag is None:
             return self.value
-        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
     
-    def __repr__(self)
+    def __repr__(self):
         return f"LeafNode(tag={self.tag}, value={self.value}, props ={self.props})"
+    
+class ParentNode(HTMLNode):
+
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
 
@@ -31,3 +31,59 @@ class TestHTMLNode(unittest.TestCase):
     def test_non_string_prop_value(self):
         node = HTMLNode(props={"data-id": 42})
         self.assertEqual(node.props_to_html(), ' data-id="42"')
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_bold(self):
+        node = LeafNode("b", "DOG")
+        self.assertEqual(node.to_html(), "<b>DOG</b>")
+
+    def test_leaf_no_value_raise(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_parent_multiple_children(self):
+        parent = ParentNode("p", [
+            LeafNode("b", "Bold"),
+            LeafNode(None, " and "),
+            LeafNode("i", "italic"),
+        ])
+        self.assertEqual(parent.to_html(), "<p><b>Bold</b> and <i>italic</i></p>")
+
+    def test_parent_nested_three_levels(self):
+        n = ParentNode("div", [
+                ParentNode("section", [
+                    ParentNode("span", [LeafNode(None, "hi")])
+                    ])
+            ])
+        self.assertEqual(n.to_html(), "<div><section><span>hi</span></section></div>")
+
+    def test_parent_child_type_error(self):
+        bad = ParentNode("div", ["not a node"])
+        with self.assertRaises(AttributeError):
+            bad.to_html()
+    
+    def test_parent_mixed_with_props_and_grandchildren(self):
+        n = ParentNode("ul", [
+            ParentNode("li", [LeafNode(None, "one")], {"class": "a"}),
+            ParentNode("li", [LeafNode("b", "two")]),
+        ], {"data-x": "1"})
+        self.assertEqual(n.to_html(), '<ul data-x="1"><li class="a">one</li><li><b>two</b></li></ul>')
