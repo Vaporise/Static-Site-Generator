04375cec90d0fc40ec66f5df89bdca2f9738f8a4
Vaporise <draxdragonfire@googlemail.com>
2025-09-09 11:42:30 +0900

    Working on function to split nodes with a delimiter

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase): # Unit test to check if 2 nodes that are equal match
@@ -27,6 +27,27 @@ class TestTextNode(unittest.TestCase): # Unit test to check if 2 nodes that are 
     def test_emptyurl(self): # Test if the url is empty that none is produced
         node = TextNode("This is a node", TextType.LINK, None)
         self.assertIsNone(node.url)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_incorrect_type(self):
+        node = TextNode("Test", "PASTA")
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
+
+    
+    def test_image(self):
+        node = TextNode("alt text here", TextType.IMAGE, url="https://example.com/cat.png")
+        html = text_node_to_html_node(node)
+
+        self.assertEqual(html.tag, "img")
+        self.assertEqual(html.value, "")
+        self.assertEqual(html.props.get("src"), "https://example.com/cat.png")
+        self.assertEqual(html.props.get("alt"), "alt text here")
 
 if __name__ == "__main__":
     unittest.main()
